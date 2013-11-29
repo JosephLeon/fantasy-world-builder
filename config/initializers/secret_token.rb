11455539acab42ac6b0d496ba00d5da291bc29eb
Joseph Leon <joseph@poeticsystems.com>
2013-11-29 15:35:41 -0600

    initial rails app commit with gem file setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldBuilder::Application.config.secret_key_base = '980842f2ff2703e59c8cbd49e8c9bfb7f84f3ec4e291c550b25c5ab8bda949ae5a808d078a2e6931fec67aa464c22df577493ecf868f4a435c9ffe41ceae0c01'
