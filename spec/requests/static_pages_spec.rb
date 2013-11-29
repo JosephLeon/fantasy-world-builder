f8300c2cd7a9c2104283173026774e8afdfd2262
Joseph Leon <joseph@poeticsystems.com>
2013-11-29 16:04:42 -0600

    change h1 on home.html.erb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
+
+  subject { page }
+
+  describe "Home Page" do
+    before { visit root_path }
+    it { should have_selector('h1', :text => 'Fantasy World Builder') }
   end
+
 end
