caffe309478c562cc19009c2e489b51c00764b06
Joseph Leon <joseph@poeticsystems.com>
2013-11-29 15:54:04 -0600

    update home.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ WorldBuilder::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
