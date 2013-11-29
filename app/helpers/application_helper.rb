20caae9eeb61a800d7475b84b89f3ca415fe9f0b
Joseph Leon <joseph@poeticsystems.com>
2013-11-29 17:29:25 -0600

    create page title helper in application_helper.rb and update code on application/help/home.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # title logic
+  def base_title(page_title)
+    base_title = "Fantasy World Builder"
+    if page_title == base_title
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
