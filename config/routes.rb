a9a4bfe9ccf9b126f78ef92f9b69e4b78712ac57
Joseph Leon <joseph@poeticsystems.com>
2013-11-29 15:59:51 -0600

    update route to make root path home.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 WorldBuilder::Application.routes.draw do
-  get "static_pages/home"
+
+  root 'static_pages#home'
+  match '/',        to: 'static_pages#home',    via: 'get'
+  #get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
