a206e202b5d59693fb08669db5b482eb8fb742e6
Joseph Leon <joseph@poeticsystems.com>
2013-11-30 00:02:06 -0600

    add menu item for characters, clean routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ WorldBuilder::Application.routes.draw do
 
   resources :characters
   root 'static_pages#home'
-  match '/',          to: 'static_pages#home',    via: 'get'
-  match '/help',      to: 'static_pages#help',    via: 'get'
+  match '/',              to: 'static_pages#home',    via: 'get'
+  match '/help',          to: 'static_pages#help',    via: 'get'
   match '/new-character', to: 'characters#new',       via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
