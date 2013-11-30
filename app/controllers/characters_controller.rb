47ad2a92611266ac3dd19386899d547139bb6d60
Joseph Leon <joseph@poeticsystems.com>
2013-11-29 23:53:49 -0600

    create index in chracters_controller.rb, create and code show.html.erb for characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,11 +1,41 @@
 class CharactersController < ApplicationController
 
+  def index
+    @character = Character.all
+  end
+
   def new
     @character = Character.new
+  end
+
+  def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to @character
+    else
+      render 'new'
+    end
   end
 
   def show
     @character = Character.find(params[:id])
   end
+
+  private
+
+    def character_params
+      params.require(:character).permit(:name,
+        :intelligence,
+        :patience,
+        :charisma,
+        :strength,
+        :endurance,
+        :agility,
+        :constitution,
+        :gold,
+        :weapon,
+        :description,
+        :history)
+    end
 
 end
