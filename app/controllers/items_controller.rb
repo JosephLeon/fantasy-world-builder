d5a877f96f6080b494d048bb30ce39c9dfa2fec7
Joseph Leon <joseph@poeticsystems.com>
2013-12-03 22:04:43 -0600

    cleaned the database and still get the error on the character/show.html.erb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,7 +32,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = @item.find(params[:id])
+    @item = Item.find(params[:id])
     @item.destroy
     redirect_to action: :index
   end
