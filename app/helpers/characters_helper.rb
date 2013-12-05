19c609f7c325073297cf4ed4d4b9441681a6044f
Joseph Leon <joseph@poeticsystems.com>
2013-12-04 20:59:33 -0600

    style selects, format gold integer to have commas, helper method tohandle pluralization on item count character show, validation on universe, errors on universe

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,2 +1,13 @@
 module CharactersHelper
+
+  def find_plurality_for_items(item_count)
+    items = "items"
+    item = "item"
+    if item_count <= 1
+      item
+    else
+      items
+    end
+  end
+
 end
--- app/models/universe.rb
+++ app/models/universe.rb
@@ -1,6 +1,10 @@
 class Universe < ActiveRecord::Base
   has_many :characters, dependent: :destroy
 
+  validates_presence_of :name, :description
+  validates_length_of :name,  :maximum => 128,
+                              :too_long => "Can't be more than 128 characters."
+
   def universe_name_for_character
     "#{name}"
   end
