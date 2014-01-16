f3919da2330965172c7d61d712bed5f4eec35a10
Joseph Leon <joseph@poeticsystems.com>
2014-01-15 22:29:12 -0600

    remove unused relationships for non existant model city

--- app/models/universe.rb
+++ app/models/universe.rb
@@ -2,7 +2,6 @@ class Universe < ActiveRecord::Base
   has_many :characters, dependent: :destroy
   has_many :areas, dependent: :destroy
   has_many :items, dependent: :destroy
-  has_many :cities, dependent: :destroy
 
   validates_presence_of :name, :description
   validates_length_of :name,  :maximum => 128,
